49f0626777b7261d1d830bddb660b9ff67fb649f
Kikkentt <kentanthonydulangon@gmail.com>
2024-10-05 20:09:31 +0800

    Added mock database using JSON

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const usersFPath = path.join(_dirname, '../data/users.json');
+
+function getUsers(){
+    const data = fs.readFileSync(usersFPath);
+    return JSON.parse(data);
+}
+
+function findUserByName(username){
+    const users = getUsers();
+    return users.find(user => user.username === username);
+}
+
+function addUser(user){
+    const users = getUsers();
+    user.id = users.length + 5;
+    users.push(user);
+    fs.writeFileSync(usersFPath, JSON.stringify(users, null, 2));
+}
+
+module.exports = {findUserByName, addUser};
