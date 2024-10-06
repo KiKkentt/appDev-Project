ab9c8772db9adb83a2d1fda3fb0bce5fe42acf7c
Kikkentt <kentanthonydulangon@gmail.com>
2024-10-06 14:59:24 +0800

    Fixed log display

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
+const {logMiddleWare} = require('./middleware/authMiddleware');
 
 const app = express();
 
+app.use(logMiddleWare);
 
 app.use(bodyParser.json());
 
 app.use('/user', userRoutes);
 
-const port = 3000;
+const port = 4000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
